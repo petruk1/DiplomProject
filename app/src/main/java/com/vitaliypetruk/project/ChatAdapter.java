6fb014c453cb6e33237e9adbdea872df8dca24a2
petruk1 <vitaliy.petruk.works.m@gmail.com>
2016-06-01 01:39:48 +0300

    ADD FILES TO PERVIOUS COMMIT

---
+++ app/src/main/java/com/vitaliypetruk/project/ChatAdapter.java
@@ -0,0 +1,86 @@
+package com.vitaliypetruk.project;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vital on 31.05.16.
+ */
+public class ChatAdapter extends BaseAdapter{
+    private static LayoutInflater inflater = null;
+    ArrayList<ChatMessage> chatMessageList;
+
+    public ChatAdapter(Activity activity, ArrayList<ChatMessage> list) {
+        chatMessageList = list;
+        inflater = (LayoutInflater) activity
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        return chatMessageList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ChatMessage message = (ChatMessage) chatMessageList.get(position);
+        View vi = convertView;
+
+        if (convertView == null)
+            if(message.isMine)
+            vi = inflater.inflate(R.layout.chatbubble2, null);
+            else vi = inflater.inflate(R.layout.chatbubble, null);
+        TextView msg = (TextView) vi.findViewById(R.id.message_text);
+        if(message.body.toCharArray().length<(message.sender.toCharArray().length+4));
+       //   msg.setWidth(message.sender.toCharArray().length+4);
+        msg.setText(message.body);
+
+        RelativeLayout layout = (RelativeLayout) vi
+                .findViewById(R.id.bubble_layout);
+
+        LinearLayout parent_layout = (LinearLayout) vi
+                .findViewById(R.id.bubble_layout_parent);
+
+        // if message is mine then align to right
+        if (message.isMine) {
+            layout.setBackgroundResource(R.drawable.bubble2);
+            parent_layout.setGravity(Gravity.RIGHT);
+
+        }
+        // If not mine then align to left
+        else {
+            layout.setBackgroundResource(R.drawable.bubble1);
+            parent_layout.setGravity(Gravity.LEFT);
+        }
+        msg.setTextColor(Color.BLACK);
+        return vi;
+    }
+
+    public void add(ChatMessage object) {
+        chatMessageList.add(object);
+    }
+}
